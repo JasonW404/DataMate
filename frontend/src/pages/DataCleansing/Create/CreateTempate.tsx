04234d93f15329c41ce95e6085a289d31e66317b
chenghh-9609 <1159096025@qq.com>
2025-10-16 11:09:54 +0800

    frontend: 修改创建清洗模版参数

--- frontend/src/pages/DataCleansing/Create/CreateTempate.tsx
+++ frontend/src/pages/DataCleansing/Create/CreateTempate.tsx
@@ -18,9 +18,8 @@ export default function CleansingTemplateCreate() {
   const handleSave = async () => {
     const template = {
       ...templateConfig,
-      operators: selectedOperators,
+      instance: selectedOperators,
     };
-    console.log("创建模板:", template);
 
     await createCleaningTemplateUsingPost(template);
     navigate("/data/cleansing");
