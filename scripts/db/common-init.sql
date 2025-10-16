32caa55b6825d8e86e79c6aeea6dc14fc7d03c3d
Dallas98 <990259227@qq.com>
2025-10-16 15:17:13 +0800

    feature: docker compose使用命名卷挂载数据

--- scripts/db/common-init.sql
+++ scripts/db/common-init.sql
@@ -3,20 +3,13 @@
 
 USE dataengine;
 
-create table if not exists t_chunk_upload_request
+CREATE TABLE IF NOT EXISTS `t_chunk_upload_request`
 (
-  id  VARCHAR(36) PRIMARY KEY COMMENT 'UUID',
-  total_file_num integer,
-  uploaded_file_num   integer,
-  upload_path   varchar(256),
-  timeout     timestamp default now(),
-  service_id varchar(64),
-  check_info   TEXT
-  );
-comment on table t_chunk_upload_request is '文件切片上传请求表';
-comment on column t_chunk_upload_request.total_file_num is '总文件数';
-comment on column t_chunk_upload_request.uploaded_file_num is '已上传文件数';
-comment on column t_chunk_upload_request.upload_path is '文件路径';
-comment on column t_chunk_upload_request.timeout is '上传请求超时时间';
-comment on column t_chunk_upload_request.service_id is '上传请求所属服务：DATA-MANAGEMENT(数据管理);';
-comment on column t_chunk_upload_request.check_info is '业务信息';
+  `id`                VARCHAR(36) PRIMARY KEY COMMENT 'UUID',
+  `total_file_num`    INT COMMENT '总文件数',
+  `uploaded_file_num` INT COMMENT '已上传文件数',
+  `upload_path`       VARCHAR(256) COMMENT '文件路径',
+  `timeout`           TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '上传请求超时时间',
+  `service_id`        VARCHAR(64) COMMENT '上传请求所属服务：DATA-MANAGEMENT(数据管理);',
+  `check_info`        TEXT COMMENT '业务信息'
+) COMMENT ='文件切片上传请求表';
